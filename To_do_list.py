0c4047b48f7161340356b911d84c75d3e6842219
Simon <61758347+SimDebug27@users.noreply.github.com>
2025-03-03 14:01:38 +0100

    Initial commit

---
+++ To_do_list.py
@@ -0,0 +1,40 @@
+listTasks = []
+input_answer = ''
+
+# Function to add a task to the list
+def add_task():
+    taskName = input('Task Name= ')
+    while True:
+        try:
+            priorityTask = int(input('Task Priority (1 and 3)= '))
+            if 1 <= priorityTask <= 3:
+                listTasks.append((taskName, priorityTask))
+                break
+            else:
+                print("Use a number between 1 and 3")
+        except:
+            print("Use a number")
+
+# Function to delete a task from the list
+def delete_task():
+    taskDelete = input("Which task would you like to delete? = ")
+    task_found = False
+    for task in listTasks:
+        if task[0] == taskDelete:
+            listTasks.remove(task)
+            task_found = True
+            break
+    if not task_found:
+        print("Task is not known in list")
+
+while input_answer != 'quit':
+    input_answer = input('What would you like to do? (Add task, Delete task or Quit)')
+    if input_answer == 'Add task':
+        add_task()
+    elif input_answer == 'Delete task':
+        delete_task()
+    elif input_answer == 'Quit':
+        print('List of Tasks:' + str(listTasks))
+        break
+    else:
+        print('Use one of the exact following Add task, Delete task or Quit')
